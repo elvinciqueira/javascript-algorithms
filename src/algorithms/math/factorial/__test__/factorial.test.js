e3f297fb69fbe1ced81fa6d29f5e6448fe1a73e7
elvinciqueira <elvinciqueira@gmail.com>
2020-05-30 22:42:19 -0300

    factorial

--- src/algorithms/math/factorial/__test__/factorial.test.js
+++ src/algorithms/math/factorial/__test__/factorial.test.js
@@ -1,5 +1,12 @@
-describe('CanaryTest', () => {
-  it('should pass this canary test', () => {
-    expect(1 + 2).toBe(3);
+import factorial from '../factorial';
+
+describe('factorial', () => {
+  it('should calculate the factorial', () => {
+    expect(factorial(0)).toBe(1);
+    expect(factorial(1)).toBe(1);
+    expect(factorial(3)).toBe(6);
+    expect(factorial(5)).toBe(120);
+    expect(factorial(8)).toBe(40320);
+    expect(factorial(10)).toBe(3628800);
   })
 })
