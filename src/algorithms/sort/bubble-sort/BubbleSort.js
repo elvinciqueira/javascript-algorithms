64da5e21fabf61bded0e04b744a0f094ab761cd2
elvinciqueira <elvinciqueira@gmail.com>
2020-06-23 19:29:58 -0300

    fix bubble sort

--- src/algorithms/sort/bubble-sort/BubbleSort.js
+++ src/algorithms/sort/bubble-sort/BubbleSort.js
@@ -4,7 +4,7 @@
   while (sorted) {
     sorted = false
 
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < input.length - 1; i++) {
       if (input[i] > input[i + 1]) {
         let temp = input[i]
 
