4a19c2a34d997ee57f0c7b849b45c0244a4ef6a8
elvinciqueira <elvinciqueira@gmail.com>
2020-08-29 10:12:50 -0300

    another one

--- src/algorithms/sort/bubble-sort/BubbleSort.js
+++ src/algorithms/sort/bubble-sort/BubbleSort.js
@@ -17,3 +17,29 @@
     }
   }
 })([6, 2, 0, 9, 1, 7, 4, 4, 8, 5, 3])
+
+// #############################################################################
+
+function bubbleSort(n) {
+
+  // O(n^2)
+  for (let outer = 0; outer < n.length; outer++) {
+    let outerElement = n[outer]
+
+    for (let inner = outer + 1; inner < n.length; inner++) {
+      let innerElement = n[inner]
+
+      if (outerElement > innerElement) {
+
+        //swap
+        n[outer] = innerElement
+        n[inner] = outerElement
+
+        //update references
+        outerElement = n[outer]
+        innerElement = n[inner]
+      }
+    }
+  }
+  return n
+}
