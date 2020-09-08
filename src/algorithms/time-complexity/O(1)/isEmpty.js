f591069258c5b04b28641eca21ee4510525294b7
elvinciqueira <elvinciqueira@gmail.com>
2020-09-08 12:15:46 -0300

    O(1)

---
+++ src/algorithms/time-complexity/O(1)/isEmpty.js
@@ -0,0 +1,14 @@
+/**
+ * Return true if an array is empty and false otherwise
+ * @param {array|string} thing
+ * @example
+ * isEmpty() // => true
+ * isEmpty([]) // => true
+ * isEmpty('') // => true
+ * isEmpty([8, 9, 6]) // => false
+ * isEmpty('text') // => false
+ */
+
+function isEmpty(thing) {
+  return !thing || thing.length < 1;
+}
