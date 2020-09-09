c97d79c84e31886ca183c0ef7e93282b24e50a61
elvinciqueira <elvinciqueira@gmail.com>
2020-09-09 16:09:48 -0300

    Searching by index takes constant time - O(1)

---
+++ src/algorithms/time-complexity/O(1)/searchByIndex.js
@@ -0,0 +1,15 @@
+/**
+ * Search for array's element by index
+ *
+ * @example Given array = [2, 5, 1, 9, 6, 7, -1];
+ * searchByIndex(array, 3); //↪️ 9
+ * searchByIndex(array, 6); //↪️ -1
+ * searchByIndex(array, 13); //↪️ undefined
+ * @param {array} array
+ * @param {number} index
+ * @returns {any} value or undefined if not found
+ */
+
+function searchByIndex(array, index) {
+  return array[index];
+}
