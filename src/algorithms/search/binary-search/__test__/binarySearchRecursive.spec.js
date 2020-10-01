25eedb79e488f349f9deac895317528d86e56024
elvinciqueira <elvinciqueira@gmail.com>
2020-10-01 12:47:44 -0300

    more cases

--- src/algorithms/search/binary-search/__test__/binarySearchRecursive.spec.js
+++ src/algorithms/search/binary-search/__test__/binarySearchRecursive.spec.js
@@ -3,6 +3,8 @@ import binarySearchRecursive from '../binarySearchRecursive';
 describe('binarySearchRecursive()', () => {
   it('should the index of the found element', () => {
     expect(binarySearchRecursive([1, 2, 3], 2)).toBe(1);
+    expect(binarySearchRecursive([1, 2, 3], 3)).toBe(2);
+    expect(binarySearchRecursive([1, 2, 3], 1)).toBe(0);
   });
 
   it('should return -1 if not found', () => {
