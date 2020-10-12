8768417485bdb8ca7f87abfb7903161f18fdf1cb
elvinciqueira <elvinciqueira@gmail.com>
2020-10-12 17:24:49 -0300

    fix

--- src/exercises/minMaxSum/minMaxSum.js
+++ src/exercises/minMaxSum/minMaxSum.js
@@ -1,6 +1,6 @@
 export const sum = values => (values || []).reduce((target, value) => target + value, 0);
 
-export function minMaxSum(arr) {
+export const minMaxSum = arr => {
   arr.sort();
 
   let maxSum = sum(arr.slice(1));
