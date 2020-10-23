767e01cc81a60e2616e57e38986b3110ed4796ab
elvinciqueira <elvinciqueira@gmail.com>
2020-10-23 14:21:32 -0300

    fix

--- src/exercises/gradingStudents/gradingStudents.js
+++ src/exercises/gradingStudents/gradingStudents.js
@@ -2,7 +2,7 @@ export default function(grades) {
   let result = 0;
 
   for (let i = 0; i < grades.length; i++) {
-    result = Math.floor(grades[i] / 5) * 5 + 5;
+    result = Math.ceil(grades[i] / 5) * 5;
 
     if (grades[i] < 38) {
       grades[i] = grades[i];
