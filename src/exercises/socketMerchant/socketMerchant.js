ff64879edb4209ba06dd74d68725f9c3df6f61ae
elvinciqueira <elvinciqueira@gmail.com>
2020-11-05 13:42:30 -0300

    solution 2 - O(n)

--- src/exercises/socketMerchant/socketMerchant.js
+++ src/exercises/socketMerchant/socketMerchant.js
@@ -1,15 +1,35 @@
 // solution 1
-export default function sockMerchant(n, arr) {
-  let sorted = arr.sort((a, b) => a - b);
-  let totalPairs = 0;
+// export default function sockMerchant(n, arr) {
+//   let sorted = arr.sort((a, b) => a - b);
+//   let totalPairs = 0;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     if (sorted[i] === sorted[i + 1]) {
+//       totalPairs++;
+
+//       i += 1;
+//     }
+//   }
+
+//   return totalPairs;
+// }
+
+
+//Solution 2 - time complexity & space complexity O(n)
+const sockMerchant = (n, arr) => {
+  let socks = {};
 
   for (let i = 0; i < n - 1; i++) {
-    if (sorted[i] === sorted[i + 1]) {
-      totalPairs++;
-
-      i += 1;
-    }
+    socks[arr[i]] ? socks[arr[i]]++ : socks[arr[i]] = 1;
   }
 
-  return totalPairs;
+  let pairs = 0;
+
+  for (let sockPair in socks) {
+    pairs += Math.floor(socks[sockPair] / 2);
+  }
+
+  return pairs;
 }
+
+export default sockMerchant;
