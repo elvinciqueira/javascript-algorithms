7ec4c1f0fbe9a55def58f6b36153c8fe45b6e1f5
elvinciqueira <elvinciqueira@gmail.com>
2020-12-01 18:54:41 -0300

    optmized Sum O(1)

--- src/exercises/optimizedSum/optimizedSum.js
+++ src/exercises/optimizedSum/optimizedSum.js
@@ -1,12 +1,34 @@
-export default function ListFastSum(arr) {
+// Brute Force O(n)
+// export default function ListFastSum(arr) {
+//   function sum(start_idx, end_idx) {
+//     let total = 0
+//     for (let i = start_idx; i < end_idx; i++) {
+//       total += arr[i]
+//     }
+
+//     return total
+//   }
+
+//   return {
+//     sum
+//   }
+// }
+
+
+// Optmized Sum time-complexity O(1), space complexity O(n)
+export default function ListFastSum(nums) {
+  let currentSum = 0
+  const sumUpTo = []
+
+  for (let i = 0; i < nums.length; i++) {
+    currentSum += nums[i]
+    sumUpTo.push(currentSum)
+  }
+
+  sumUpTo.push(0)
+
   function sum(start_idx, end_idx) {
-    let total = 0
-
-    for (let i = start_idx; i < end_idx; i++) {
-      total += arr[i]
-    }
-
-    return total
+    return sumUpTo[end_idx - 1] - sumUpTo[start_idx - 1]
   }
 
   return {
