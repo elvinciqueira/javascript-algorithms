fd49642192672892ee39bae1bed2c7ad72c78219
elvinciqueira <elvinciqueira@gmail.com>
2020-12-28 17:11:33 -0300

    fix

--- src/exercises/twoSum/__tests__/twoSum.spec.js
+++ src/exercises/twoSum/__tests__/twoSum.spec.js
@@ -8,8 +8,8 @@ describe('twoSum()', () => {
   // });
 
   it('should given an array of integers nums and an integer target, return the two numbers such that they add up to target.', () => {
-    // expect(twoSum([4, 1, 2, -2, 11, 15, 1, -1, -6, -4], 9)).toEqual([-2, 11]);
-    // expect(twoSum([4, 1, 2, 15, 1, -1, -6, -4], 9)).toEqual([15, -6]);
+    expect(twoSum([4, 1, 2, -2, 11, 15, 1, -1, -6, -4], 9)).toEqual([-2, 11]);
+    expect(twoSum([4, 1, 2, 15, 1, -1, -6, -4], 9)).toEqual([15, -6]);
     expect(twoSum([4, 1, 2, 1, -1, -4], 9)).toEqual([]);
   });
 });
