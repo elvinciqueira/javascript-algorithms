731c200c4d60530aad93278d0b104ff79696a460
elvinciqueira <elvinciqueira@gmail.com>
2021-07-29 13:35:44 -0300

    refactor

--- src/exercises/max_min/maxMin.js
+++ src/exercises/max_min/maxMin.js
@@ -1,6 +1,14 @@
 export function maxMin(A) {
-  const min = Math.min(...A);
-  const max = Math.max(...A);
-
+  let len = A.length;
+  let max = A[0];
+  let min = A[0];
+  for (let i = 1; i < len; i++) {
+    if (A[i + 1] > max) {
+      max = A[i + 1];
+    }
+    if (A[i + 1] < min) {
+      min = A[i + 1];
+    }
+  }
   return max + min;
 }
