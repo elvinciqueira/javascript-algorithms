6737d6a8268e1eba7df0d18a017c74aab90d8202
elvinciqueira <elvinciqueira@gmail.com>
2021-07-29 13:37:07 -0300

    fix

--- src/exercises/max_min/maxMin.js
+++ src/exercises/max_min/maxMin.js
@@ -3,11 +3,11 @@ export function maxMin(A) {
   let max = A[0];
   let min = A[0];
   for (let i = 1; i < len; i++) {
-    if (A[i + 1] > max) {
-      max = A[i + 1];
+    if (A[i] > max) {
+      max = A[i];
     }
-    if (A[i + 1] < min) {
-      min = A[i + 1];
+    if (A[i] < min) {
+      min = A[i];
     }
   }
   return max + min;
