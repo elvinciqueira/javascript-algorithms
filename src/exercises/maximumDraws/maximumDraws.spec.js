0d101475bb0ccaee1e499a9165a5c4c3996989ef
elvinciqueira <elvinciqueira@gmail.com>
2021-08-02 21:37:22 -0300

    fix

--- src/exercises/maximumDraws/maximumDraws.spec.js
+++ src/exercises/maximumDraws/maximumDraws.spec.js
@@ -2,7 +2,7 @@ import { maximumDraws } from './maximumDraws';
 
 describe('maximumDraws()', () => {
   describe('A person is getting ready to leave and needs a pair of matching socks. If there are  colors of socks in the drawer, how many socks need to be removed to be certain of having a matching pair?', () => {
-    describe('There are  colors of socks in the drawer. If they remove  socks, they may not match.', () => {
+    describe('There are 2 colors of socks in the drawer. If they remove  socks, they may not match.', () => {
       test('The minimum number to insure success is 3', () => {
         expect(maximumDraws(1)).toBe(2);
         expect(maximumDraws(9999999)).toBe(10000000);
