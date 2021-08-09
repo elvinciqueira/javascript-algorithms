7e6b08841de1479011f79ddbcadb90cf4d318ab1
elvinciqueira <elvinciqueira@gmail.com>
2021-08-09 14:48:07 -0300

    two sum Solutions

--- src/exercises/diagonalDifference/diagonalDifference.spec.js
+++ src/exercises/diagonalDifference/diagonalDifference.spec.js
@@ -2,6 +2,12 @@ import diagonalDifference from './diagonalDifference';
 
 describe('diagonalDifference', () => {
   it('should given a square matrix, calculate the absolute difference between the sums of its diagonals.', () => {
-    expect(diagonalDifference([[1,2,3], [4,5,6], [7,8,9]])).toBe(2);
+    expect(
+      diagonalDifference([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ]),
+    ).toBe(0);
   });
 });
--- src/exercises/twoSum/twoSum.js
+++ src/exercises/twoSum/twoSum.js
@@ -4,18 +4,50 @@
  * @return {number[]}
  */
 
+//Solution 1 - O(n^2)
+// export default function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     const first = nums[i];
+//     for (let j = i + 1; j < nums.length - 1; j++) {
+//       const second = nums[j];
+//       if (first + second === target) {
+//         return [first, second];
+//       }
+//     }
+//   }
+//   return [];
+// }
+
+// Solution 2 - O(n), Space O(n)
 export default function twoSum(nums, target) {
-  const calc_nums = {};
-
-  for (const n of nums) {
-    let diff = target - n;
-
-    if (diff in calc_nums) {
-      return [diff, n];
+  const calc_numbers = {};
+  for (let num of nums) {
+    const result = target - num;
+    if (result in calc_numbers) {
+      return [result, num];
     } else {
-      calc_nums[n] = true;
+      calc_numbers[num] = true;
     }
   }
-
   return [];
 }
+
+//Solution 3 - O(n log(n))
+// export default function twoSum(nums, target) {
+//   nums.sort((a, b) => a - b);
+//   let leftPointer = 0;
+//   let rightPointer = nums.length - 1;
+//   while (leftPointer < rightPointer) {
+//     const result = nums[leftPointer] + nums[rightPointer];
+//     if (result == target) {
+//       return [nums[leftPointer], nums[rightPointer]];
+//     } else if (result < target) {
+//       leftPointer += 1;
+//     } else if (result > target) {
+//       rightPointer -= 1;
+//     }
+//   }
+//   return [];
+// }
+
+twoSum([4, 1, 2, -2, 11, 16, 1, -1, -6, -4], 9); //?
