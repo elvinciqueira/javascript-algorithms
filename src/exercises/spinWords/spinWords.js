02ccd5f3de13fe88c1a9994a267334defc487c17
elvinciqueira <elvinciqueira@gmail.com>
2021-09-02 23:03:59 -0300

    refactor

--- src/exercises/spinWords/spinWords.js
+++ src/exercises/spinWords/spinWords.js
@@ -1,6 +1,8 @@
 export function spinWords(string) {
   const stringToArray = string.split(' ');
+  const hasFiveOrMoreLetters = word => word.length >= 5;
+  const spinWord = word => word.split('').reverse().join('');
   return stringToArray
-    .map(word => (word.length >= 5 ? word.split('').reverse().join('') : word))
+    .map(word => (hasFiveOrMoreLetters(word) ? spinWord(word) : word))
     .join(' ');
 }
