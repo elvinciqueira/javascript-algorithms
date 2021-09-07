932c2bd6c442b8d91c324fd11c88530c92b551cd
elvinciqueira <elvinciqueira@gmail.com>
2021-09-07 11:03:22 -0300

    refactor tests

--- src/exercises/arrayDiff/arrayDiff.spec.js
+++ src/exercises/arrayDiff/arrayDiff.spec.js
@@ -1,13 +1,13 @@
-import { arrayDiff } from './arrayDiff';
+import { array_diff } from './array_diff';
 
-describe('arrayDiff()', () => {
+describe('array_diff()', () => {
   describe('It should remove all values from list a, which are present in list b keeping their order.', () => {
-    test('arrayDiff([1,2],[1]) == [2]', () => {
-      expect(arrayDiff([1, 2], [1])).toEqual([2]);
+    test('array_diff([1,2],[1]) == [2]', () => {
+      expect(array_diff([1, 2], [1])).toEqual([2]);
     });
 
-    test('arrayDiff([1,2,2,2,3],[2]) == [1,3]', () => {
-      expect(arrayDiff([1, 2, 2, 2, 3], [2])).toEqual([1, 3]);
+    test('array_diff([1,2,2,2,3],[2]) == [1,3]', () => {
+      expect(array_diff([1, 2, 2, 2, 3], [2])).toEqual([1, 3]);
     });
   });
 });
