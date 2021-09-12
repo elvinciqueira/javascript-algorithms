0641f80ec7d47a0e2b47a5c24d8c3e1df12c345b
elvinciqueira <elvinciqueira@gmail.com>
2021-09-12 17:03:58 -0300

    refactor

--- src/exercises/buyAndSellStocks/maxProfit.js
+++ src/exercises/buyAndSellStocks/maxProfit.js
@@ -2,11 +2,11 @@ export const maxProfit = prices => {
   let maxProfit = 0;
   let minPrice = prices[0];
   for (const price of prices) {
-    const difference = price - minPrice;
+    const profit = price - minPrice;
     if (price < minPrice) {
       minPrice = price;
-    } else if (difference > maxProfit) {
-      maxProfit = difference;
+    } else if (profit > maxProfit) {
+      maxProfit = profit;
     }
   }
   return maxProfit;
