8b051452ab88d31220126ed2e901043114260a5d
elvinciqueira <elvinciqueira@gmail.com>
2021-09-12 18:33:33 -0300

    refactor: my own split function

--- src/exercises/reverseString/reverseString.js
+++ src/exercises/reverseString/reverseString.js
@@ -1,11 +1,29 @@
+const _split = str => {
+  const array = [];
+  let index = 0;
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] === ' ') {
+      if (index !== j) {
+        array.push(str.slice(index, j));
+      }
+      index = j + 1;
+    }
+  }
+  array.push(str.slice(index, str.length));
+  return array;
+};
+
 export const reverseString = A => {
-  const str = A.split(' ');
+  const stringToArray = _split(A);
   let left = 0;
-  let right = str.length - 1;
+  let right = stringToArray.length - 1;
   while (left < right) {
-    [str[left], str[right]] = [str[right], str[left]];
+    [stringToArray[left], stringToArray[right]] = [
+      stringToArray[right],
+      stringToArray[left],
+    ];
     left++;
     right--;
   }
-  return str.join(' ').trim();
+  return stringToArray.join(' ').trim();
 };
