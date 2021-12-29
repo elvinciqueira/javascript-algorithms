fc5d1881eb6ea4b4bb2df6aed53e932be84100c0
elvinciqueira <elvinciqueira@gmail.com>
2021-12-29 19:30:45 -0300

    refactor

--- src/exercises/missingLetter/missingLetter.js
+++ src/exercises/missingLetter/missingLetter.js
@@ -1,43 +1,14 @@
 export function missingLetter(str) {
-  const alphabet = {
-    a: true,
-    b: true,
-    c: true,
-    d: true,
-    e: true,
-    f: true,
-    g: true,
-    h: true,
-    i: true,
-    j: true,
-    k: true,
-    l: true,
-    m: true,
-    n: true,
-    o: true,
-    p: true,
-    q: true,
-    r: true,
-    s: true,
-    t: true,
-    u: true,
-    v: true,
-    w: true,
-    x: true,
-    y: true,
-    z: true,
-  };
+  const hashtable = {};
 
   for (let i = 0; i < str.length; i++) {
-    const letter = str.charAt(i);
-    if (alphabet[letter]) {
-      alphabet[letter] = false;
-    }
+    hashtable[str[i]] = true;
   }
 
-  for (const letter in alphabet) {
-    if (alphabet[letter] === true) {
-      return letter;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < alphabet.length; i++) {
+    if (!hashtable[alphabet[i]]) {
+      return alphabet[i];
     }
   }
 }
