ed1c6a54392f329ec07998cd516a7f3c9ad5a860
elvinciqueira <elvinciqueira@gmail.com>
2022-03-15 14:43:05 -0300

    refactor

--- src/exercises/search/search.js
+++ src/exercises/search/search.js
@@ -5,15 +5,9 @@ export const search = (nums, target) => {
   while (startIndex <= endIndex) {
     const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
 
-    if (nums[middleIndex] === target) {
-      return middleIndex;
-    }
-
-    if (nums[middleIndex] < target) {
-      startIndex = middleIndex + 1;
-    } else {
-      endIndex = middleIndex - 1;
-    }
+    if (nums[middleIndex] === target) return middleIndex;
+    if (nums[middleIndex] < target) startIndex = middleIndex + 1;
+    else endIndex = middleIndex - 1;
   }
 
   return -1;
