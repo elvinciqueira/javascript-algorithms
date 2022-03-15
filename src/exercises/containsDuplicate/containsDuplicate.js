4535587e4f821ce5c0daf61472f42274e32aad32
elvinciqueira <elvinciqueira@gmail.com>
2022-03-15 15:43:52 -0300

    refactor: contains duplicate

--- src/exercises/containsDuplicate/containsDuplicate.js
+++ src/exercises/containsDuplicate/containsDuplicate.js
@@ -1,9 +1,11 @@
 export const containsDuplicate = arr => {
-  const sortedArr = arr.sort((a, b) => a - b);
+  const hash = {};
 
-  for (let i = 0; i < sortedArr.length - 1; i++) {
-    if (sortedArr[i] === sortedArr[i + 1]) {
+  for (let i = 0; i < arr.length; i++) {
+    if (hash[arr[i]]) {
       return true;
+    } else {
+      hash[arr[i]] = true;
     }
   }
 
