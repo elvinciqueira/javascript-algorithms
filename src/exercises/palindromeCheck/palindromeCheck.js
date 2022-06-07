7bfbb8e14ee4b424cffe42c34cd1fa0b294cd2e6
elvinciqueira <elvinciqueira@gmail.com>
2022-06-07 09:58:58 -0300

    reverse string in javascript

--- src/exercises/palindromeCheck/palindromeCheck.js
+++ src/exercises/palindromeCheck/palindromeCheck.js
@@ -13,16 +13,21 @@
 // }
 
 // O(n) time, space O(1)
-export default function palindromeCheck(str) {
-  let leftPointer = str.length - 1;
-  for (let rightPointer = 0; rightPointer < str.length; rightPointer++) {
-    if (leftPointer === rightPointer) {
-      return true;
-    }
-    if (str[leftPointer] !== str[rightPointer]) {
-      return false;
-    }
-    leftPointer -= 1;
-  }
-  return true;
-}
+// export default function palindromeCheck(str) {
+//   let leftPointer = str.length - 1;
+//   for (let rightPointer = 0; rightPointer < str.length; rightPointer++) {
+//     if (leftPointer === rightPointer) {
+//       return true;
+//     }
+//     if (str[leftPointer] !== str[rightPointer]) {
+//       return false;
+//     }
+//     leftPointer -= 1;
+//   }
+//   return true;
+// }
+
+// O(N) time, Space O(1)
+// export default function palindromeCheck(str) {
+//   return [...str].reverse().join('') === str;
+// }
